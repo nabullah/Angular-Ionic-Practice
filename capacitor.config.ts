246a302a6355a6e1a70f10c651ddf8cfabb521c3
Nabullah Ansari <nansari@anviam.com>
2023-07-31 11:33:03 +0530

    changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,30 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'todoList',
+  appId: 'com.demo.todoList',
+  appName: 'Ionic Demo',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      // launchAutoHide: true,
+      // launchFadeOutDuration: 3000,
+      backgroundColor: '#000',
+      // androidSplashResourceName: 'splash',
+      // androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      androidSpinnerStyle: 'small',
+      iosSpinnerStyle: 'small',
+      // spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: 'launch_screen',
+      // useDialog: true,
+    },
+  },
 };
 
 export default config;
